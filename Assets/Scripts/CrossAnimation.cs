c96911638922e5ce9515b39dd65f50a37163e716
JacobMacPro <wuhhoward7562180@gmail.com>
2017-10-24 15:30:12 +0800

    script, object

---
+++ Assets/Scripts/CrossAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossAnimation : MonoBehaviour {
+
+	public GameObject upCur,downCur,leftCur,rightCur;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			upCur.GetComponent<Animator>().enabled = true;
+			downCur.GetComponent<Animator>().enabled=true;
+			leftCur.GetComponent<Animator>().enabled=true;
+			rightCur.GetComponent<Animator>().enabled=true;
+			StartCoroutine( WaitingAnim () );
+		}
+	}
+
+	IEnumerator WaitingAnim(){
+		float time = 0.1f;
+		yield return new WaitForSeconds (time);
+		upCur.GetComponent<Animator> ().enabled = false;
+		downCur.GetComponent<Animator> ().enabled = false;
+		leftCur.GetComponent<Animator> ().enabled = false;
+		rightCur.GetComponent<Animator> ().enabled = false;
+	}
+}
---
+++ Assets/Scripts/GlobalAmmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAmmo : MonoBehaviour {
+	
+	public static int currentAmmo;
+	public GameObject ammoDisplay;
+
+	private int internalAmmo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		internalAmmo = currentAmmo;
+		ammoDisplay.GetComponent<Text>().text = "Ammo " + internalAmmo;
+	}
+}
